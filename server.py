d99be40020639c3fc3ccb7f98403d25b1223a4b1
root <root@alarmpi.b.qotw.net>
2014-02-12 01:43:46 -0700

    Fixed Python 3 issues and added reset method.

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 controller = webpixels.ColorKinetics(host="192.168.1.44")
 
 pixels = []
-for i in xrange(8):
+for i in range(8):
    r, g, b = controller.channels[i*3:i*3+3]
    pixels.append(webpixels.RgbPixel(r, g, b))
 
@@ -21,6 +21,13 @@ def pixel_set(pixel, color):
    color = int(color, 16)
    r, g, b = color >> 16 & 255, color >> 8 & 255, color & 255
    pixels[pixel].set(r, g, b)
+   controller.sync()
+   return ''
+
+@app.route('/reset')
+def pixel_reset():
+   for pixel in pixels:
+      pixel.set(0, 0, 0)
    controller.sync()
    return ''
 
